5a1b02ebf58e4a6c1cb79589388f5203bbd7efeb
waltzaround <vxs4050@autuni.ac.nz>
2015-10-07 11:32:21 +1300

    lol
    pushing all the files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;using System.Collections;public class PlayerController : MonoBehaviour{    public float speed;    private Rigidbody rb;    private int count;    void Start ()    {        rb = GetComponent<Rigidbody>();        count = 0;    }    void FixedUpdate ()    {        float moveHorizontal = Input.GetAxis("Horizontal");        float moveVertical = Input.GetAxis("Vertical");        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);        rb.AddForce(movement * speed);    }    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+    }}
